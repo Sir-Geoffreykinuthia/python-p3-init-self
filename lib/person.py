fce349fa76d1106b1a06a5c2504ca1ff19a5b5c1
geoffrey kinuthia <geoffreykinuthia10@gmail.com>
2023-05-22 18:40:30 +0300

    Done

--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 class Person:
-    pass
+    # dunder method very important
+    def __init__(self, name):
+
+        self.name = name
+
+    def talk(self):
+        
+        print("Hello World!") 
+
+    def walk(self):
+
+        print("The person is walking.")       
